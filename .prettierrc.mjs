a9934f53bc14a78e659ec558e597761bf5939449
ras0q <ras0q@users.noreply.github.com>
2024-02-04 21:26:06 +0900

    :wrench: ingroduce prettier and eslint
    https://zenn.dev/t4aru/scraps/dc2b570fec1c5a

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
