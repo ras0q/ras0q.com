fd42cdd7c9341732c8df1ed5d3793f023f72dcbc
ras0q <ras0q@users.noreply.github.com>
2024-02-05 21:59:10 +0900

    :recycle: move consts

---
+++ src/libs/consts.ts
@@ -0,0 +1,37 @@
+export const domain = 'ras0q.com'
+
+export const subDomains: {
+  name: string
+  description: string
+}[] = [
+  {
+    name: 'slitscan3d',
+    description: 'Three.jsを活用したSlit-Scanアプリ',
+  },
+  {
+    name: 'foo',
+    description: 'foo',
+  },
+  {
+    name: 'bar',
+    description: 'bar',
+  },
+]
+
+export const subRoutes: {
+  path: string
+  description: string
+}[] = [
+  {
+    path: '/about',
+    description: 'About me',
+  },
+  {
+    path: '/works',
+    description: 'Works',
+  },
+  {
+    path: '/contact',
+    description: 'Contact',
+  },
+]
