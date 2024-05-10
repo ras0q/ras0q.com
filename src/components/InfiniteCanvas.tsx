c47c993d38d94a7026a219b2429ecce235a7d3a4
ras0q <ras0q@users.noreply.github.com>
2024-05-10 22:35:22 +0900

    :boom: place absolutely

--- src/components/InfiniteCanvas.tsx
+++ src/components/InfiniteCanvas.tsx
@@ -6,23 +6,12 @@ import styled, { css } from 'styled-components'
 
 const Container = styled.div`
   height: 100vh;
-  width: auto;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 6rem;
-`
-
-const CardsContainer = styled.div`
-  max-width: 150px;
-  display: flex;
-  flex-direction: column;
-  gap: 5rem;
+  width: 100vw;
+  position: relative;
+  overflow: hidden;
 `
 
 const draggableStyle = css`
-  position: relative;
   cursor: grab;
   &:active {
     cursor: grabbing;
@@ -56,6 +45,10 @@ export const InfiniteCanvas = () => {
   const subDomainsRefs = subDomains.map(() => useRef<HTMLAnchorElement>(null))
   const subRoutesRefs = subRoutes.map(() => useRef<HTMLAnchorElement>(null))
   const draggableRefs: RefObject<HTMLElement>[] = [titleRef, ...subDomainsRefs, ...subRoutesRefs]
+  const draggablePositions = [domain, ...subDomains, ...subRoutes].map(({ left, top }) => ({
+    originalLeft: left,
+    originalTop: top,
+  }))
 
   const nowOffset = (el: HTMLElement) => {
     return {
@@ -65,12 +58,17 @@ export const InfiniteCanvas = () => {
   }
 
   useEffect(() => {
-    draggableRefs.forEach((ref) => {
+    draggableRefs.forEach((ref, i) => {
+      const container = containerRef.current
       const el = ref.current
-      if (!el) return
+      if (!container || !el) return
 
       // save original position for damping oscillation
-      const { nowLeft: originalLeft, nowTop: originalTop } = nowOffset(el)
+      const { originalLeft, originalTop } = draggablePositions[i]
+
+      el.style.position = 'absolute'
+      el.style.left = `${originalLeft}px`
+      el.style.top = `${originalTop}px`
 
       el.addEventListener('pointermove', (e) => {
         if (e.buttons === 1) {
@@ -79,11 +77,10 @@ export const InfiniteCanvas = () => {
           const top = nowTop + e.movementY
 
           // document.body.style.touchAction = 'none'
-          el.style.left = `${left - originalLeft}px`
+          el.style.left = `${left}px`
           el.style.right = 'auto'
-          el.style.top = `${top - originalTop}px`
+          el.style.top = `${top}px`
           el.style.bottom = 'auto'
-          el.style.position = 'relative'
           el.draggable = false
           el.setPointerCapture(e.pointerId)
         }
@@ -106,8 +103,8 @@ export const InfiniteCanvas = () => {
           const t = ts - ts0
           const dx = (left0 - originalLeft) * Math.exp(-b * t) * Math.cos(omega * t)
           const dy = (top0 - originalTop) * Math.exp(-b * t) * Math.cos(omega * t)
-          el.style.left = `${dx}px`
-          el.style.top = `${dy}px`
+          el.style.left = `${originalLeft + dx}px`
+          el.style.top = `${originalTop + dy}px`
 
           requestAnimationFrame(returnToOriginal)
         }
@@ -118,37 +115,48 @@ export const InfiniteCanvas = () => {
 
   return (
     <Container ref={containerRef}>
-      <CardsContainer key="subDomains">
-        {subDomains.map(({ name, description }, i) => (
-          <Fragment key={name}>
-            <Connector fromRef={subDomainsRefs[i]} toRef={titleRef} />
-            <DraggableLinkCard
-              aRef={subDomainsRefs[i]}
-              title={name}
-              body={description}
-              href={`https://${name}.${domain}`}
-            />
-          </Fragment>
-        ))}
-      </CardsContainer>
+      <Title ref={titleRef}>{domain.name}</Title>
 
-      <Title ref={titleRef} key="title">
-        ras0q.com
-      </Title>
+      {subDomains.map(({ name, description }, i) => (
+        <Fragment key={name}>
+          <Connector fromRef={subDomainsRefs[i]} toRef={titleRef} />
+          <DraggableLinkCard
+            aRef={subDomainsRefs[i]}
+            title={name}
+            body={description}
+            href={`https://${name}.${domain.name}`}
+          />
+        </Fragment>
+      ))}
 
-      <CardsContainer key="subRoutes">
-        {subRoutes.map(({ path, description }, i) => (
-          <Fragment key={path}>
-            <Connector fromRef={subRoutesRefs[i]} toRef={titleRef} r2l />
-            <DraggableLinkCard
-              aRef={subRoutesRefs[i]}
-              title={path}
-              body={description}
-              href={path}
-            />
-          </Fragment>
-        ))}
-      </CardsContainer>
+      {subRoutes.map(({ path, description }, i) => (
+        <Fragment key={path}>
+          <Connector fromRef={subRoutesRefs[i]} toRef={titleRef} r2l />
+          <DraggableLinkCard aRef={subRoutesRefs[i]} title={path} body={description} href={path} />
+        </Fragment>
+      ))}
+
+      {/* <button
+        style={{ position: 'absolute' }}
+        onClick={() => {
+          const container = containerRef.current
+          if (!container) return
+
+          const data = draggableRefs.map((ref) => {
+            const el = ref.current
+            if (!el) return { left: 0, top: 0 }
+
+            return {
+              left: el.style.left,
+              top: el.style.top,
+            }
+          })
+
+          console.log(data)
+        }}
+      >
+        save
+      </button> */}
     </Container>
   )
 }
--- src/libs/consts.ts
+++ src/libs/consts.ts
@@ -1,37 +1,47 @@
-export const domain = 'ras0q.com'
+export const domain = {
+  name: 'ras0q.com',
+  left: 345,
+  top: 276,
+}
 
-export const subDomains: {
-  name: string
-  description: string
-}[] = [
+export const subDomains = [
   {
     name: 'slitscan3d',
     description: 'Three.jsを活用したSlit-Scanアプリ',
+    left: 36,
+    top: 55,
   },
   {
     name: 'foo',
     description: 'foo',
+    left: 59,
+    top: 269,
   },
   {
     name: 'bar',
     description: 'bar',
+    left: 22,
+    top: 520,
   },
 ]
 
-export const subRoutes: {
-  path: string
-  description: string
-}[] = [
+export const subRoutes = [
   {
     path: '/about',
     description: 'About me',
+    left: 778,
+    top: 43,
   },
   {
     path: '/works',
     description: 'Works',
+    left: 862,
+    top: 275,
   },
   {
     path: '/contact',
     description: 'Contact',
+    left: 897,
+    top: 570,
   },
 ]
