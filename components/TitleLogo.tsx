499bd84d84a49356c21e9e16710805510932a764
ras0q <ras0q@users.noreply.github.com>
2024-08-29 11:37:31 +0900

    :bug: fix check

--- components/TitleLogo.tsx
+++ components/TitleLogo.tsx
@@ -1,20 +1,23 @@
 // @ts-types="../static/styled-system/css/index.d.mts"
-import { css } from "../static/styled-system/css/index.mjs";
+import { css, cx } from "../static/styled-system/css/index.mjs";
 
 export const TitleLogo = (props: { class?: string }) => (
   <span
-    class={css`
-      font-style: italic;
-      font-weight: 800;
-      margin: 0;
-      padding-right: 0.25rem; /* prevent text cut */
-      user-select: none;
-      display: inline-block;
-      background: linear-gradient(120deg, var(--ctp-latte-lavender), var(--ctp-latte-pink));
-      background-clip: text;
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    ` + " " + props.class}
+    class={cx(
+      css`
+        font-style: italic;
+        font-weight: 800;
+        margin: 0;
+        padding-right: 0.25rem; /* prevent text cut */
+        user-select: none;
+        display: inline-block;
+        background: linear-gradient(120deg, var(--ctp-latte-lavender), var(--ctp-latte-pink));
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      `,
+      props.class,
+    )}
   >
     ras0q.com
   </span>
