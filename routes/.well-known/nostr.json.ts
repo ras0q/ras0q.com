7718653a8f470e0892282aca7e9a22f1ba36eb26
ras0q <ras0q@users.noreply.github.com>
2024-08-29 11:45:33 +0900

    :recycle: allow any name

--- routes/.well-known/nostr.json.ts
+++ routes/.well-known/nostr.json.ts
@@ -1,11 +1,23 @@
 import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
-  GET(req) {
+  GET(req, ctx) {
+    const name = ctx.params["name"];
+    if (!name) {
+      return new Response(
+        JSON.stringify({
+          message: "name is required",
+        }),
+        {
+          status: 400,
+        },
+      );
+    }
+
     return new Response(
       JSON.stringify({
         "names": {
-          "_":
+          [name]:
             "1f60f02ded6e17bf5f5e419049bbc2907709047946a8fd4a29ca776ea539fddf",
         },
       }),
