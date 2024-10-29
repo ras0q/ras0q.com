4223a56fe8fba902834c3df97375436d35270e94
ras0q <ras0q@users.noreply.github.com>
2024-10-29 22:18:23 +0900

    ✨ Add account icons

--- islands/InfiniteCanvasIsland.tsx
+++ islands/InfiniteCanvasIsland.tsx
@@ -16,6 +16,37 @@ export default function InfiniteCanvasIsland() {
     top: 276,
   };
 
+  const icons = [
+    {
+      name: "X",
+      link: "https://x.com/ras0q",
+      path: "/img/x.svg",
+      left: 550,
+      top: 250,
+    },
+    {
+      name: "GitHub",
+      link: "https://github.com/ras0q",
+      path: "/img/github.svg",
+      left: 600,
+      top: 250,
+    },
+    {
+      name: "Bluesky",
+      link: "https://bsky.app/ras0q.com",
+      path: "/img/bluesky.svg",
+      left: 650,
+      top: 250,
+    },
+    {
+      name: "Nostr",
+      link: "https://njump.me/ras0q.com",
+      path: "/img/nostr.svg",
+      left: 700,
+      top: 250,
+    },
+  ];
+
   const subDomains = [
     {
       name: "slitscan3d.",
@@ -65,6 +96,14 @@ export default function InfiniteCanvasIsland() {
       <Draggable id={title.id} left={title.left} top={title.top}>
         <TitleLogo class={css`font-size: 5rem;`} />
       </Draggable>
+
+      {icons.map(({ name, link, path, left, top }) => (
+        <Draggable id={name} left={left} top={top}>
+          <a href={link}>
+            <img src={path} alt={name} width="32px" height="32px" />
+          </a>
+        </Draggable>
+      ))}
 
       {subDomains.map((v) => <Connector leftID={v.id} rightID={title.id} />)}
       {subDomains.map(({ id, name, description, left, top }) => (
